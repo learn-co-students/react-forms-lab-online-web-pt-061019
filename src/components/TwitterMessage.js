606bc5f8f9c5e256f8d35ff0fff2ef145352e77c
scrabill <shannon.crabill@gmail.com>
2020-03-22 16:31:36 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleEvent = (event) => {
+    this.setState({
+      message: event.target.value
+    })
+    // event.preventDefault()
+    // console.log("Typing...")
+    // console.log(event.target.value.length) // poodle = 6
+    // this.setState({
+    //   maxChars: event.target.value.count
+    // }))
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleEvent} value={this.state.message} />
+        <strong>Characters remaining:</strong> {this.props.maxChars - this.state.message.length}
       </div>
     );
   }
